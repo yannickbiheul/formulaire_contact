9fd6b9503686d2cad6d0f091121034d0e9f129fb
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-13 10:52:54 +0200

    Création des variables

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
-// VARIABLES
+    /* VARIABLES */
+// Prénom
 let casePrenom = document.getElementById('prenom');
 let aidePrenom = document.getElementById('aidePrenom');
 let userPrenom = "";
+// Nom
+let caseNom = document.getElementById('nom');
+let aideNom = document.getElementById('aideNom');
+let userNom = "";
+// Email
+let caseEmail = document.getElementById('email');
+let aideEmail = document.getElementById('aideEmail');
+let userEmail = "";
+// Tel
+let caseTel = document.getElementById('tel');
+let aideTel = document.getElementById('aideTel');
+let userTel = "";
+// Message
+let caseMessage = document.getElementById('message');
+let aideMessage = document.getElementById('aideMessage');
+let userMessage = "";
+
 
 // VERIFICATION PRENOM
 casePrenom.addEventListener('blur', function(e) {
     userPrenom = e.target.value;
-    if (e.target.value.length < 3) {
+    if (userPrenom.length < 3) {
         aidePrenom.style.color = "#EA2027";
         aidePrenom.textContent = "Prénom trop court !";
     } else {
